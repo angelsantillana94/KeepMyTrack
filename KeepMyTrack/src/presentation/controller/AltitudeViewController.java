9997b65f74b576685568b65a0f0b628f3648126f
angelsantillana94 <angelsantillana94@gmail.com>
2016-05-09 13:12:14 +0200

    added AltitudeView and MainView

---
+++ KeepMyTrack/src/presentation/controller/AltitudeViewController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentation.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author angelsantillana
+ */
+public class AltitudeViewController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    void initStage(Stage newStage) {
+         //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- KeepMyTrack/src/presentation/controller/MainViewController.java
+++ KeepMyTrack/src/presentation/controller/MainViewController.java
@@ -1,16 +1,21 @@
 package presentation.controller;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javax.xml.bind.JAXBException;
 import jgpx.model.analysis.TrackData;
@@ -23,19 +28,16 @@ public class MainViewController implements Initializable {
 
     private Stage stage;
     private TrackData trackData;
-    
-      @FXML
-    private TableView<?> tableActivities;
-    @FXML
-    private TableColumn<?, ?> nameActivity;
-    @FXML
-    private TableColumn<?, ?> dateActivity;
     @FXML
     private Button btnAdd;
     @FXML
     private Button btnDel;
     @FXML
-    private Label titleActivity;
+    private TableView<?> tableActivities;
+    @FXML
+    private TableColumn<?, ?> nameActivity;
+    @FXML
+    private TableColumn<?, ?> dateActivity;
     @FXML
     private Label distance;
     @FXML
@@ -66,6 +68,9 @@ public class MainViewController implements Initializable {
     private Button btnStatistics;
     @FXML
     private Button btnZones;
+    @FXML
+    private Label labelNameActivity;
+   
 
     public void initStage(Stage stage) {
         this.stage = stage;
@@ -76,7 +81,8 @@ public class MainViewController implements Initializable {
 
     }
 
-    private void loadGPX(ActionEvent event) throws JAXBException {
+    @FXML
+    private void addActivity(ActionEvent event) throws JAXBException  {
         FileChooser fileChooser = new FileChooser();
         File file = fileChooser.showOpenDialog(stage);
         if (file == null) return; 
@@ -85,15 +91,25 @@ public class MainViewController implements Initializable {
     }
 
     @FXML
-    private void addActivity(ActionEvent event) {
-    }
-
-    @FXML
     private void deleteActivity(ActionEvent event) {
     }
 
     @FXML
     private void showAltitude(ActionEvent event) {
+        try {
+            Stage newStage = new Stage();
+            newStage.setTitle("Perfil del recorrido");
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/presentation/view/AltitudeView.fxml"));
+            AnchorPane root = (AnchorPane) loader.load();
+            Scene scene = new Scene(root);
+            newStage.setScene(scene);
+            AltitudeViewController avc = loader.getController();
+            avc.initStage(newStage); 
+            newStage.initModality(Modality.APPLICATION_MODAL);
+            newStage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -103,5 +119,6 @@ public class MainViewController implements Initializable {
     @FXML
     private void showZones(ActionEvent event) {
     }
+
 
 }
