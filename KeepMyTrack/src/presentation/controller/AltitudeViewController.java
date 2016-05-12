69222d604f4cd90b80ce368c274028cb4e051d71
josegarm <josegarm94@gmail.com>
2016-05-12 12:10:45 +0200

    Changes in AltitudeView

--- KeepMyTrack/src/presentation/controller/AltitudeViewController.java
+++ KeepMyTrack/src/presentation/controller/AltitudeViewController.java
@@ -7,7 +7,6 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.AreaChart;
-import javafx.scene.chart.StackedAreaChart;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 import jgpx.model.analysis.Chunk;
@@ -24,27 +23,27 @@ public class AltitudeViewController implements Initializable {
     private Activity activity;
 
     @FXML
-    private StackedAreaChart<Number, Number> areaChart;
+    private AreaChart<Number, Number> areaChart;
 
     public void initStage(Stage stage, Activity activity) {
         this.stage = stage;
         this.activity = activity;
         XYChart.Series series = new XYChart.Series();
         areaChart.getData().addAll(series);
-        areaChart.setCreateSymbols(false);
         ObservableList<Chunk> chunks = activity.getChunks();
         double distance = 0;
+        double duration = 0;
         for(Chunk chunk : chunks){
             series.getData().add(new XYChart.Data(distance, chunk.getAvgHeight()));
             distance += chunk.getDistance()/1000;
+            //duration += chunk.getDuration().getSeconds()/60.0;
         }
         
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        /*
-        */
+        
     }
 
 }
